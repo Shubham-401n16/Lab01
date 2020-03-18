9ceadccb77be483109a8b3c23f4a6c86c9d7defd
smajum dar22 <smajumdar22@gmail.com>
2020-03-17 17:38:52 -0700

    In Progress

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 const input = require('./lib/input.js');
 const notes = require('./lib/notes.js');
 
-if (input.validateInput.payload) {
-    console.log('Valid input');
-};
+if (input.validateInput.payload) console.log('Valid input');
+else console.log('Error');
 
 // eslint-disable-next-line no-console
 console.log(notes(input));
